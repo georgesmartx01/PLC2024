1f7d371a368d450124b01caa24acbfec0e69b586
georgesmartx01 THEBEST <copilaugeorgeandrei87@gmail.com>
2024-01-29 16:52:26 +0000

    update: lab2-functionalprimer/haskell/task1.hs

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,9 +3,11 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+     print (MyFunc2 10)
+     print (3rdFunc "abc")
+     print (4thFunc "xyz")
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+MyFunc2 x = x*2
+3rdFunc myStr = str + "foo"
+4thFunc myStr = str + "bar"
